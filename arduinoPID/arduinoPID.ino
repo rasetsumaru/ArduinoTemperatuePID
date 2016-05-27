ae3f0af3f523c5b50d66791e3f6494c58feff067
rasetsumaru <rasetsumaru@icloud.com>
2016-05-27 15:29:00 -0300

    Update text

--- arduinoPID/arduinoPID.ino
+++ arduinoPID/arduinoPID.ino
@@ -181,7 +181,7 @@ void loop(void) {
       Serial.print(modelabel[mode - 1]);
       Serial.print('\n');
   
-      Serial.print(F("Digite um valor para entre 20.00 e 60.00 para configurar o setpoint."));
+      Serial.print(F("Digite um valor entre 20.00 e 60.00 para configurar o setpoint."));
       Serial.print('\n');
     }
     else{
@@ -208,7 +208,7 @@ void loop(void) {
       Serial.print('\n');
       Serial.println(F("O valor digitado para o setpoint esta fora da escala."));
       Serial.println(F("Por favor, digite um valor válido."));
-      Serial.println(F("Digite um valor para entre 20.00 e 60.00 para configurar o setpoint."));
+      Serial.println(F("Digite um valor entre 20.00 e 60.00 para configurar o setpoint."));
       
     }
         
